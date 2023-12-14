51eaf975e4c3615b0847402a3bcf9038697b6769
kracelh542 <kracelh542@gmail.com>
2023-12-14 10:43:01 +0100

    Inicio del proyecto

---
+++ src/main/java/daw/Ej1.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author khalid
+ */
+public class Ej1 {
+
+    public static void main(String[] args) {
+        
+   
+        
+      //do{
+      // try{
+      String numPalabras = JOptionPane.showInputDialog(null, 
+                  "Introduce el numero de palabras a guardar");
+           
+      //}catch(){
+          
+      //}
+      //}while()
+  
+  
+        
+        
+    }
+}
