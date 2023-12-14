f6f24fae2a9969818555377c87dba8f158de8c42
kracelh542 <kracelh542@gmail.com>
2023-12-14 11:16:58 +0100

    Parte 1 echa

--- src/main/java/daw/Ej1.java
+++ src/main/java/daw/Ej1.java
@@ -1,9 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package daw;
-
 
 import javax.swing.JOptionPane;
 
@@ -14,21 +12,31 @@ import javax.swing.JOptionPane;
 public class Ej1 {
 
     public static void main(String[] args) {
-        
-   
-        
-      //do{
-      // try{
-      String numPalabras = JOptionPane.showInputDialog(null, 
-                  "Introduce el numero de palabras a guardar");
-           
-      //}catch(){
-          
-      //}
-      //}while()
-  
-  
-        
-        
+        System.out.println(palabrasLeidas());
     }
+
+    public static int palabrasLeidas() {
+
+        boolean error = true;
+        String numPalabras = "";
+        int numeroPal=0;
+
+        do {
+
+            try {
+                numPalabras = JOptionPane.showInputDialog(null,
+                        "Introduce el numero de palabras a guardar");
+                numeroPal = Integer.parseInt(numPalabras);
+                error = false;
+            } catch (NumberFormatException exc) {
+                JOptionPane.showMessageDialog(null, "Has introducido un dato incorrecto");
+                error = true;
+            }
+            
+
+        } while (error);
+
+        return numeroPal;
+    }
+
 }
